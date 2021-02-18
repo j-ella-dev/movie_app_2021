aafbd32515e7d9c83699bff1c99dcc22bf7053de
금영 정 <kyjung@mcscience.com>
2021-02-18 13:34:25 +0900

    최최종 코드

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../components/Movie.css';
 
 //리다이렉트 기능 : route props의 history 키 활용
 //history키에는 push, go, goBack, goForward 등이 있음
@@ -13,7 +14,24 @@ class Detail extends React.Component {
     render() {
         const { location } = this.props;
         if (location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="movie">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                    <div className="movie__data">
+                        <h3 className="movie__title">
+                            {location.state.title}
+                        </h3>
+                        <h5 className="movie__year">{location.state.year}</h5>
+                        {/* <ul className="movie__genre">
+                            {genres.map((genre, index) => { //index를 빼면 key porp 에러 메시지 발생
+                                return <li key={index} className="movie_genre">{genres}</li>;
+                            })}
+                        </ul> */}
+                        {/* 시놉시스 180자까지만 자르기 */}
+                        <p className="movie__summary">{location.state.summary}</p>
+                    </div>
+                </div>
+            );
         } else {
             return null;
         }
